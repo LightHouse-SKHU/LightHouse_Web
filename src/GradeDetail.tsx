57901a179553946d07493448c503b9ad2b3b23e7
Younghyun Kim <dana_03@naver.com>
2024-04-08 02:38:38 +0900

    grade id update

--- src/GradeDetail.tsx
+++ src/GradeDetail.tsx
@@ -1,103 +1,108 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 interface QuestInfo {
-    id: string;
-    title: string;
-    content: string;
-    multipleChoice: string;
-    score: number;
-    grade: string;
-    category: string;
-    correct: string;
-    correctPercentage: number;
-    imgPath: string;
+  id: string;
+  title: string;
+  content: string;
+  multipleChoice: string;
+  score: number;
+  grade: string;
+  category: string;
+  correct: string;
+  correctPercentage: number;
+  imgPath: string;
 }
 
 const GradeDetail: React.FC = () => {
-    const [quest, setQuest] = useState<QuestInfo[]>([]);
-    const navigate = useNavigate();
+  const [quest, setQuest] = useState<QuestInfo[]>([]);
+  const { id } = useParams<{ id: string }>();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const fetchQuestData = async () => {
-            try {
-                // localstorage에 저장했던 토큰 가져오기
-                const token = localStorage.getItem('token');
+  useEffect(() => {
+    const fetchQuestData = async () => {
+      try {
+        // localstorage에 저장했던 토큰 가져오기
+        const token = localStorage.getItem("token");
 
-                // 헤더에 토큰 추가
-                const config = {
-                    headers: {
-                        Authorization: `Bearer ${token}`
-                    }
-                };
-
-                // 서버에 사용자 정보 달라고 get 요청 보내기
-                const response = await axios.get(`https://lighthouse1.site/examples/all`, config);
-                setQuest(response.data);  // 요청 완료시 reponse변수에 서버에서 받은 사용자 정보가 저장될 것
-
-            } catch (error) { // get 실패시 console 메시지 출력
-                console.error('Error fetching data:', error);
-                // navigate('/Login')
-            }
+        // 헤더에 토큰 추가
+        const config = {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         };
 
-        fetchQuestData();
-    }, [navigate]);  // id 추가
+        // 서버에 사용자 정보 달라고 get 요청 보내기
+        const response = await axios.get(
+          `https://lighthouse1.site/examples/${id}`,
+          config
+        );
+        setQuest(response.data); // 요청 완료시 reponse변수에 서버에서 받은 사용자 정보가 저장될 것
+      } catch (error) {
+        // get 실패시 console 메시지 출력
+        console.error("Error fetching data:", error);
+        // navigate('/Login')
+      }
+    };
 
-    if (!quest) {  // quest가 null인 경우 로딩 표시
-        return <div>Loading...</div>;
-    }
+    fetchQuestData();
+  }, [navigate]); // id 추가
 
-    const QuestionWrite = () => {
-        navigate('/QuestionWrite');
-    }
+  if (!quest) {
+    // quest가 null인 경우 로딩 표시
+    return <div>Loading...</div>;
+  }
 
-    return (
-        <>
-        <div className="background">
-        <div className="leftNav">
-          <Link to='/Board' className="Nav" id="board1">전체 문제</Link>
-          <hr />
-          <Link to='/Board' className="Nav" id="board2">1학년 문제</Link><br />
-          <Link to='/Board' className="Nav">2학년 문제</Link><br />
-          <Link to='/Board' className="Nav">3학년 문제</Link>
-        </div>
+  const QuestionWrite = () => {
+    navigate("/QuestionWrite");
+  };
+
+  return (
+    <>
+      <div className="background">
         <div>
-            <h1>전체 문제</h1>
-            <div className="middleList">
-                <table>
-                    <thead>
-                        <tr className="boardTitle">
-                            <th>No.</th>
-                            <th>Title</th>
-                            <th>Content</th>
-                            <th>MultipleChoice</th>
-                            <th>Img</th>
-                            <th>Category</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {quest.map((data: QuestInfo) => (
-                            <Link to={`/get/${data.id}/${data.category}`} className="boardContent">
-                                <td>{data.id}</td>
-                                <td>{data.title}</td>
-                                <td>{data.content}</td>
-                                <td>{data.multipleChoice}</td>
-                                <td><img src={data.imgPath} alt={data.imgPath} /></td>
-                                <td>{data.category}</td>
-                            </Link>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+          <h1>전체 문제</h1>
+          <div className="middleList">
+            <table>
+              <thead>
+                <tr className="boardTitle">
+                  <th>No.</th>
+                  <th>Title</th>
+                  <th>Content</th>
+                  <th>MultipleChoice</th>
+                  <th>Img</th>
+                  <th>Category</th>
+                </tr>
+              </thead>
+              <tbody>
+                {quest.map((data: QuestInfo) => (
+                  <Link
+                    to={`/get/${data.id}/${data.category}`}
+                    className="boardContent"
+                  >
+                    <td>{data.id}</td>
+                    <td>{data.title}</td>
+                    <td>{data.content}</td>
+                    <td>{data.multipleChoice}</td>
+                    <td>
+                      <img src={data.imgPath} alt={data.imgPath} />
+                    </td>
+                    <td>{data.category}</td>
+                  </Link>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-        <div className='boardBtn'>
-        <button onClick={QuestionWrite} className='writeBtn'>글 작성</button>
+        <div className="boardBtn">
+          <button onClick={QuestionWrite} className="writeBtn">
+            글 작성
+          </button>
         </div>
-        </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default GradeDetail;
