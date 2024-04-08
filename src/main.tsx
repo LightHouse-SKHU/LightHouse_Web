56bbb7758250063269ed85d79503cef7b62f61e2
Younghyun Kim <dana_03@naver.com>
2024-04-09 01:03:31 +0900

    vercel error4

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from "react-dom/client";
-import App from "./Main/App.tsx";
+import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import Header from "./Main/Header.tsx";
+import Header from "./Header.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
